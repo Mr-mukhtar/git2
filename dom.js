cb88590d968fc1e0787726ec12d969e08ceeeaca
mr-mukhtar <mukhtarmohd517@gmail.com>
2023-07-15 19:56:28 +0530

    changes in head and color

---
+++ dom.js
@@ -0,0 +1,5 @@
+var items = document.getElementById('main-header');
+items.style.borderBottom = 'solid 3px black';
+var item = document.getElementsByClassName('list-group-item');
+item[0].style.fontWeight = 'bold';
+item[0].style.backgroundColor = 'green';

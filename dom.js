ecb83be3cb2f4dc4f0cd63649cbf2999e9cc3733
mr-mukhtar <mukhtarmohd517@gmail.com>
2023-07-16 10:49:09 +0530

    bold color font

--- dom.js
+++ dom.js
@@ -2,4 +2,9 @@ var items = document.getElementById('main-header');
 items.style.borderBottom = 'solid 3px black';
 var item = document.getElementsByClassName('list-group-item');
 item[0].style.fontWeight = 'bold';
-item[0].style.backgroundColor = 'green';
+item[2].style.backgroundColor = 'green';
+item[0].innerHTML = 'helloo';
+
+for (let i=0; i<item.length; i++){
+    item[i].style.fontWeight= 'bold';
+}

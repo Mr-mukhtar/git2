193517d624de373ff6e8484e0a4f82228cad85a0
mr-mukhtar <mukhtarmohd517@gmail.com>
2023-07-16 11:12:46 +0530

    new li element without the same class Name

--- dom.js
+++ dom.js
@@ -1,5 +1,6 @@
-var items = document.getElementById('main-header');
-items.style.borderBottom = 'solid 3px black';
+// var items = document.getElementById('main-header');
+// items.style.borderBottom = 'solid 3px black';
+//by class name
 var item = document.getElementsByClassName('list-group-item');
 item[0].style.fontWeight = 'bold';
 item[2].style.backgroundColor = 'green';
@@ -8,3 +9,12 @@ item[0].innerHTML = 'helloo';
 for (let i=0; i<item.length; i++){
     item[i].style.fontWeight= 'bold';
 }
+//by tag name
+// var li = document.getElementsByTagName('li');
+// li[0].style.fontWeight = 'bold';
+// li[2].style.backgroundColor = 'green';
+// li[0].innerHTML = 'helloo';
+
+// for (let i=0; i<li.length; i++){
+//     li[i].style.fontWeight= 'bold';
+// }
